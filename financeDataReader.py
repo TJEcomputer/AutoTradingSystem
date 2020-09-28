439b17c710bfb4f5c2050ad47a5dbed1c65124e7
Hector / Shin JoongHyeok <AIshinjoonghyeok@gmail.com>
2020-09-28 11:07:52 +0900

    Add files via upload
    financeDataReader
    news.py(invest.com)

---
+++ financeDataReader.py
@@ -0,0 +1,54 @@
+# import FinanceDataReader as fdr
+# import numpy as np
+# import pandas as pd
+
+# ###상장종목 전체####
+# df_krx = fdr.StockListing('KRX')
+# print(df_krx)
+# df_krx.to_csv('krx.csv')
+#
+# ####Kospi####
+# df_kospi = fdr.StockListing('kospi')
+# print(df_kospi)
+# df_kospi.to_csv('KOSPI.csv')
+#
+# ####Kosdaq####
+# df_kosdaq = fdr.StockListing('kosdaq')
+# print(df_kosdaq)
+# df_kosdaq.to_csv('KOSDAQ.csv')
+#
+# ####Nasdaq####
+# df_nasdaq = fdr.StockListing('Nasdaq')
+# print(df_nasdaq)
+# df_nasdaq.to_csv('NASDAQ.csv')
+
+# ####Nyse####
+# df_nyse = fdr.StockListing('NYSE')
+# print(df_nyse)
+# df_nyse.to_csv('NYSE.csv')
+
+# ####Amex####
+# df_amex = fdr.StockListing('AMEX')
+# print(df_amex)
+# df_amex.to_csv('AMEX.csv')
+
+# ####S&P500####
+# df_sp500 = fdr.StockListing('SP500')
+# print(df_sp500)
+# df_sp500.to_csv('SP500.csv')
+
+# ####삼성전자####
+# df_samsung = fdr.DataReader('005930', '2000')
+# print(df_samsung)
+# df_samsung.to_csv('samsung.csv')
+#
+# ####코스피지수####
+# df_kospi_index = fdr.DataReader('KS11', '2000') #2000년~현재
+# print(df_kospi_index)
+# df_kospi_index.to_csv('kospi_index.csv')
+#
+# ####환율####
+# df_won_rate = fdr.DataReader('USD/KRW', '2000') #2000년~현재
+# print(df_won_rate)
+# df_won_rate.to_csv('won_rate.csv')
+
