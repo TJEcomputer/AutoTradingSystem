e304470f13620bddab09f45529e16ef62f99a7cb
Hector / Shin JoongHyeok <AIshinjoonghyeok@gmail.com>
2020-10-16 16:58:49 +0900

    Add files via upload

---
+++ 신중혁/investpy.py
@@ -0,0 +1,11 @@
+# import investpy
+# import numpy as np
+# import pandas as pd
+#
+# df = investpy.get_stock_historical_data(stock='AAPL',
+#                                         country='United States',
+#                                         from_date='01/01/1997',
+#                                         to_date='22/09/2020')
+# print(df)
+
+##################jupyter notebook###############
